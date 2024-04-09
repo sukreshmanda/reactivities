4cefd012323e12b947f630e9a151b8c928d0282a
sukreshmanda <mandasukresh@gmail.com>
2024-04-09 17:26:31 +0530

    [Sukresh] Add Documentation to update linter

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 ## Run application
 1. Clone the project
 2. run `dotnet restore`
-3. run `dotnet ef database update` inside `API` folder
-4. run `dotnet run` to start app
+3. run `dotnet run` to start app
+
+## Run DB migrations
+1. run `dotnet ef database update` inside `API` folder
+
+## Format project files
+1. run `dotnet format style -v detailed --severity info`
